f11dcf90b01c0d88069daeeb289988531f248e4f
Luan@YokKok <97419663+LuanPhan2911@users.noreply.github.com>
2025-02-09 16:07:04 +0700

    click to move

--- Assets/Scripts/Systems/UnitMoverSystem.cs
+++ Assets/Scripts/Systems/UnitMoverSystem.cs
@@ -11,20 +11,23 @@ partial struct UnitMoverSystem : ISystem
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        foreach (var (localTrasform, moveSpeed, velocity) in
-            SystemAPI.Query<RefRW<LocalTransform>, RefRO<MoveSpeed>, RefRW<PhysicsVelocity>>())
+        foreach (var (localTrasform, unitMover, velocity) in
+            SystemAPI.Query<RefRW<LocalTransform>, RefRO<UnitMover>, RefRW<PhysicsVelocity>>())
         {
-            float3 targetPosition = MouseWorldPosition.Instance.GetPosition();
-            float3 moveDirecttion = targetPosition - localTrasform.ValueRO.Position;
+
+
+            float3 moveDirecttion = unitMover.ValueRO.targtePosition - localTrasform.ValueRO.Position;
+
             moveDirecttion = math.normalize(moveDirecttion);
 
-            float rotateSpeed = 5f;
+
 
             localTrasform.ValueRW.Rotation =
                 math.slerp(localTrasform.ValueRW.Rotation,
-                quaternion.LookRotation(moveDirecttion, math.up()), rotateSpeed * SystemAPI.Time.DeltaTime);
+                quaternion.LookRotation(moveDirecttion, math.up()),
+                unitMover.ValueRO.rotateSpeed * SystemAPI.Time.DeltaTime);
 
-            velocity.ValueRW.Linear = moveDirecttion * moveSpeed.ValueRO.value;
+            velocity.ValueRW.Linear = moveDirecttion * unitMover.ValueRO.moveSpeed;
 
             // can automaticly rotate
             velocity.ValueRW.Angular = float3.zero;
